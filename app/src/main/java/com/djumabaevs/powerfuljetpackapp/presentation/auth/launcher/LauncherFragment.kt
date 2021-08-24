a6df380da62bef35720c9d0ee0505f1c46ccdd7b
Djumabaevs <djumabaevb@gmail.com>
2021-08-24 17:38:13 +0600

    register fragment

--- app/src/main/java/com/djumabaevs/powerfuljetpackapp/presentation/auth/launcher/LauncherFragment.kt
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/presentation/auth/launcher/LauncherFragment.kt
@@ -20,6 +20,7 @@ class LauncherFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_launcher, container, false)
     }
