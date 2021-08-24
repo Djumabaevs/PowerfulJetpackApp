eaf2a397f15e3caeaf6aa1d3111fb07397ab9412
Djumabaevs <djumabaevb@gmail.com>
2021-08-24 17:38:01 +0600

    frgot password fragment

---
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/presentation/auth/forgot_password/ForgotPasswordFragment.kt
@@ -0,0 +1,27 @@
+package com.djumabaevs.powerfuljetpackapp.presentation.auth.forgot_password
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.djumabaevs.powerfuljetpackapp.R
+
+
+class ForgotPasswordFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+}
