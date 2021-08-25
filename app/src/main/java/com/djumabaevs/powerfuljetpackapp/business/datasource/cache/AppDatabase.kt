7f72a0886d122204c72b737a068bf6e32ef1db59
Djumabaevs <djumabaevb@gmail.com>
2021-08-25 15:24:40 +0600

    AppDatabase : RoomDatabase

--- app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/datasource/cache/AppDatabase.kt
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/datasource/cache/AppDatabase.kt
@@ -1,7 +1,25 @@
 package com.djumabaevs.powerfuljetpackapp.business.datasource.cache
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.djumabaevs.powerfuljetpackapp.business.datasource.cache.account.AccountDao
+import com.djumabaevs.powerfuljetpackapp.business.datasource.cache.account.AccountEntity
+import com.djumabaevs.powerfuljetpackapp.business.datasource.cache.auth.AuthTokenDao
+import com.djumabaevs.powerfuljetpackapp.business.datasource.cache.auth.AuthTokenEntity
+import com.djumabaevs.powerfuljetpackapp.business.datasource.cache.blog.BlogPostEntity
 
+@Database(entities = [AuthTokenEntity::class, AccountEntity::class, BlogPostEntity::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountDao
+
+    abstract fun getBlogPostDao(): BlogPostDao
+
+    companion object{
+        val DATABASE_NAME: String = "app_db"
+    }
+
 
 }
