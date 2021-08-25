3d8478b3fea3750a5367e31a9978173a79699cff
Djumabaevs <djumabaevb@gmail.com>
2021-08-25 17:57:39 +0600

    generic response

---
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/datasource/network/GenericResponse.kt
@@ -0,0 +1,13 @@
+package com.djumabaevs.powerfuljetpackapp.business.datasource.network
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class GenericResponse(
+    @SerializedName("response")
+    @Expose
+    val response: String?,
+
+    @SerializedName("error_message")
+    val errorMessage: String?
+)
