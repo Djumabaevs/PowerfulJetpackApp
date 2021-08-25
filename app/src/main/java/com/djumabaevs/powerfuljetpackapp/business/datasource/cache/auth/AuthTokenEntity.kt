f429811127145df65b0bc12bd942755aa546d389
Djumabaevs <djumabaevb@gmail.com>
2021-08-25 10:06:54 +0600

    @Entity AuthTokenEntity

--- app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/datasource/cache/auth/AuthTokenEntity.kt
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/datasource/cache/auth/AuthTokenEntity.kt
@@ -1,4 +1,53 @@
 package com.djumabaevs.powerfuljetpackapp.business.datasource.cache.auth
 
-class AuthTokenEntity {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.djumabaevs.powerfuljetpackapp.business.datasource.cache.account.AccountEntity
+import com.djumabaevs.powerfuljetpackapp.business.domain.models.AuthToken
+import com.google.gson.annotations.Expose
+
+
+@Entity(
+    tableName = "auth_token",
+    foreignKeys = [
+        ForeignKey(
+            entity = AccountEntity::class,
+            parentColumns = ["pk"],
+            childColumns = ["account_pk"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class AuthTokenEntity(
+
+    @PrimaryKey
+    @ColumnInfo(name = "account_pk")
+    var account_pk: Int? = -1,
+
+
+    @ColumnInfo(name = "token")
+    @Expose
+    val token: String? = null
+)
+
+fun AuthTokenEntity.toAuthToken(): AuthToken {
+    if(account_pk == null){
+        throw Exception("Account PK cannot be null.")
+    }
+    if(token == null){
+        throw Exception("Token cannot be null.")
+    }
+    return AuthToken(
+        accountPk = account_pk!!,
+        token = token,
+    )
+}
+
+fun AuthToken.toEntity(): AuthTokenEntity{
+    return AuthTokenEntity(
+        account_pk = accountPk,
+        token = token,
+    )
 }
