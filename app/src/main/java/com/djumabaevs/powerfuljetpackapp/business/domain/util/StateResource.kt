45b226eaf262b2bc6ef86a8006d1e09f1eb1a386
Djumabaevs <djumabaevb@gmail.com>
2021-08-26 10:50:07 +0600

    Response data class

--- app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/domain/util/StateResource.kt
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/business/domain/util/StateResource.kt
@@ -3,3 +3,8 @@ package com.djumabaevs.powerfuljetpackapp.business.domain.util
 
 data class StateMessage(val response: Response)
 
+data class Response(
+    val message: String?,
+    val uiComponentType: UIComponentType,
+    val messageType: MessageType
+)
