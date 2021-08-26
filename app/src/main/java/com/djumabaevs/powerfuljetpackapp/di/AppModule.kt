9941857bb9d9079974c0fb4a3772c36b7ad4840c
Djumabaevs <djumabaevb@gmail.com>
2021-08-26 11:19:01 +0600

    provide retrofit builder

--- app/src/main/java/com/djumabaevs/powerfuljetpackapp/di/AppModule.kt
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/di/AppModule.kt
@@ -3,10 +3,15 @@ package com.djumabaevs.powerfuljetpackapp.di
 import android.app.Application
 import com.djumabaevs.powerfuljetpackapp.business.datasource.datastore.AppDataStore
 import com.djumabaevs.powerfuljetpackapp.business.datasource.datastore.AppDataStoreManager
+import com.djumabaevs.powerfuljetpackapp.business.domain.util.Constants
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -19,5 +24,20 @@ object AppModule {
         application: Application
     ): AppDataStore {
         return AppDataStoreManager(application)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson {
+        return GsonBuilder()
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitBuilder(gsonBuilder: Gson): Retrofit.Builder{
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
     }
 }
