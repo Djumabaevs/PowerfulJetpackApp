611f8d3086420eaddd99f5457e7fe1775a429dc2
Djumabaevs <djumabaevb@gmail.com>
2021-08-30 13:42:01 +0600

    data store keys

--- app/src/main/java/com/djumabaevs/powerfuljetpackapp/presentation/session/SessionManager.kt
+++ app/src/main/java/com/djumabaevs/powerfuljetpackapp/presentation/session/SessionManager.kt
@@ -1,9 +1,13 @@
 package com.djumabaevs.powerfuljetpackapp.presentation.session
 
+import androidx.lifecycle.MutableLiveData
 import com.djumabaevs.powerfuljetpackapp.business.datasource.datastore.AppDataStore
 import javax.inject.Inject
 import javax.inject.Singleton
 import com.djumabaevs.powerfuljetpackapp.presentation.session.SessionEvents.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Think of this class as an "application viewmodel"
@@ -17,4 +21,17 @@ class SessionManager
     private val logout: Logout,
     private val appDataStoreManager: AppDataStore,
 ) {
+    private val TAG: String = "AppDebug"
+
+    private val sessionScope = CoroutineScope(Dispatchers.Main)
+
+    val state: MutableLiveData<SessionState> = MutableLiveData(SessionState())
+
+    init {
+        //Check if a user was authenticated in a previous session
+
+        sessionScope.launch {
+            appDataStoreManager.readValue(DataStoreKe)
+        }
+    }
 }
